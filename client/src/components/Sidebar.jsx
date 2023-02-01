39de17a2fa237cbf08fe200f23fee2bf3fa7eb6c
JCaceres <juancaceres9812@gmail.com>
2023-02-01 00:21:17 -0300

    sos un boludo

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import {
+    SettingOutlined,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutliend,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMounthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined
+} from "@mui/icons-material";
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import FlexBetween from './FlexBetween';
+import profileImage from "assets/profile.jpeg"
+
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <HomeOutlined />
+    },
+    {
+        text: "Client Facing",
+        icon: null,
+    },
+    {
+        text: "Products",
+        icon: <ShoppingCartOutlined />
+    },
+    {
+        text: "Customers",
+        icon: <Groups2Outlined />
+    },
+    {
+        text: "Transactions",
+        icon: <ReceiptLongOutliend />
+    },
+    {
+        text: "Geography",
+        icon: <PublicOutlined />
+    },
+    {
+        text: "Sales",
+        icon: null,
+    },
+    {
+        text: "Overview",
+        icon: <PointOfSaleOutlined />
+    },
+    {
+        text: "Daily",
+        icon: <TodayOutlined />
+    },
+    
+]
+
+export const Sidebar = ({
+    drawerWidth,
+    isSidebarOpen,
+    setIsSidebarOpen,
+    isNonMobile,
+}) => {
+    const { pathname } = useLocation();
+    const [active, setActive] = useState("");
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    useEffect(() => {
+        setActive(pathname.substring(1));
+    }, [pathname])    
+
+  return <Box component="nav">
+    {isSidebarOpen && (
+        <Drawer 
+            open={isSidebarOpen}
+            onClose={() => setIsSidebarOpen(false)}
+            variant="persistent"
+            anchor="left"
+            sx={{
+                width: drawerWidth,
+                "& .MuiDrawer-paper": {
+                    color: theme.palette.secondary[200],
+                    background: theme.palette.background.alt,
+                    boxSizing: "border-box",
+                    borderWidth: isNonMobile ? 0 : "2px",
+                    width: drawerWidth
+                }
+            }}
+        >
+            <Box width="100%">
+                <Box m="1.5rem 2rem 2rem 3rem">
+                    <FlexBetween color={theme.palette.secondary.main}>
+                        <Box display="flex" alingItems="center" gap="0.5rem">
+                            <Typography variante="h4" fontWeight="bold">
+                                ECOMVISION
+                            </Typography>
+                        </Box>
+                        {!isNonMobile && (
+                            <IconButton onClick={() => setIsSidebarOpen (!isSidebarOpen)}>
+                                <ChevronLeft />
+                            </IconButton>
+                        )}
+                    </FlexBetween>
+                </Box>
+                <List>
+                    {}
+                </List>
+            </Box>
+        </Drawer>
+    )}
+  </Box>;
+};
